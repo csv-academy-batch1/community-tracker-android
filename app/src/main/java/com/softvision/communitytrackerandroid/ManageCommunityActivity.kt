40b8435ee3049b3a70dc0a95232c78f0a70ea930
SVPH\shalom.pel <shalom.pel@softvision.com>
2022-07-12 19:58:43 +0800

    created UI for Create Community Screen

--- app/src/main/java/com/softvision/communitytrackerandroid/ManageCommunityActivity.kt
+++ app/src/main/java/com/softvision/communitytrackerandroid/ManageCommunityActivity.kt
@@ -3,40 +3,68 @@ package com.softvision.communitytrackerandroid
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import android.widget.AdapterView
-import android.widget.ArrayAdapter
-import android.widget.Spinner
-import android.widget.TextView
+import android.widget.*
+import androidx.appcompat.app.AlertDialog
+import com.softvision.communitytrackerandroid.databinding.ActivityMainBinding
+import com.softvision.communitytrackerandroid.databinding.ActivityManageCommunityBinding
 
 class ManageCommunityActivity : AppCompatActivity() {
 
     lateinit var community: Spinner
-    lateinit var result: TextView
+   // lateinit var result: TextView
+
+    private lateinit var binding: ActivityManageCommunityBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_manage_community)
+        binding = ActivityManageCommunityBinding.inflate(layoutInflater)
 
-        community = findViewById(R.id.sp_community) as Spinner
-        result = findViewById(R.id.tv_result) as TextView
 
-        val communities = arrayOf("Enterprise.Net", "Full-stack Web", "Quality Engineer", "Cloud and DevOps", "Big Data and Analytics",
-        "Product Delivery", "Enterprise Coffee", "Mobile", "Research and Developmen")
+        setContentView(binding.root)
 
-        community.adapter = ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,communities)
+        community = findViewById(R.id.spcommunity) as Spinner
+        //result = findViewById(R.id.tv_result) as TextView
 
-        community.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
-            override fun onNothingSelected(parent: AdapterView<*>?) {
-                result.text = "Please Select on Option"
+        // val communities = arrayOf("Enterprise.Net", "Full-stack Web", "Quality Engineer", "Cloud and DevOps", "Big Data and Analytics",
+        // "Product Delivery", "Enterprise Coffee", "Mobile", "Research and Developmen")
+
+//        val spinner : Spinner = findViewById(R.id.spinner)
+//        ArrayAdapter.createFromResource(
+//            this@ManageCommunityActivity,
+//            R.array.community_array,
+//            R.layout.custom_simple_spinner_item
+//        ).also { adapter ->
+//            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+//            spinner.adapter = adapter
+//        }
+        val adapter = ArrayAdapter.createFromResource(
+            this@ManageCommunityActivity,
+            R.array.community_array,
+            android.R.layout.simple_spinner_item
+        )
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        binding.spcommunity.adapter = adapter
+
+
+        with(binding) {
+            val spinner = spcommunity.selectedItem.toString()
+            button.setOnClickListener{
+                val builder: AlertDialog.Builder? = this@ManageCommunityActivity.let {
+                    AlertDialog.Builder(it)
+
+                }
+                builder?.setTitle("Community Name Saved: ${etNameOfCommunity.text.toString()}")
+                    ?.setMessage("Assigned To: $spinner.\n\n\n Description: ${etCommunityDescription.text.toString()}")
+
+                val dialog: AlertDialog? = builder?.create()
+                dialog?.show()
             }
 
-            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
-                 result.text = communities.get(position)
             }
-
         }
     }
-}
+
+
 
 
 
