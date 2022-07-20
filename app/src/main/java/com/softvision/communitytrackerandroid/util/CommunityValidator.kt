8ee84c37c40de404edd1857fda05d581ecd9f0bd
Neil Nicoli Mosca <neilnicoli.mosca@softvision.com>
2022-07-20 21:04:04 +0800

    Update CommunityValidator.kt
    Remove CommunityResquest class reference

--- app/src/main/java/com/softvision/communitytrackerandroid/util/CommunityValidator.kt
+++ app/src/main/java/com/softvision/communitytrackerandroid/util/CommunityValidator.kt
@@ -1,11 +1,10 @@
 package com.softvision.communitytrackerandroid.util
 
 import com.softvision.communitytrackerandroid.data.model.Community
-import com.softvision.communitytrackerandroid.data.model.CommunityRequest
 
 object CommunityValidator {
 
-    fun validateCommunity(community: CommunityRequest) : Boolean {
+    fun validateCommunity(community: Community) : Boolean {
         return community.name.isNotEmpty() && community.managerId > 0
     }
 }
